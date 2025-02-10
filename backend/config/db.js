e93e6f8dca9e59aa118754e9f404c3ba72b39c74
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2025-02-10 15:14:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config(); // Load environment variables
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'Tiger',
-    database: process.env.DB_NAME || 'restaurantmanagementsystem'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 // Connect to MySQL
