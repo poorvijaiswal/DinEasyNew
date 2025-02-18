b0d5f25743e212ec486d1601dc61b852935a2c0e
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2025-02-18 20:46:02 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -93,7 +93,7 @@ export default function Login() {
         </div>
 
         <div className="w-1/2 p-6">
-          <h2 className="text-3xl font-bold mb-4">Staff Login</h2>
+          <h2 className="text-3xl font-bold mb-4">Login</h2>
           {message && <p className="text-green-600 text-center">{message}</p>}
           
 
